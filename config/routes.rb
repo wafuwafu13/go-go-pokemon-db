26c4046d96552f3ca3bcc53f417260fd179ad940
wafuwafu13 <mariobaske@i.softbank.jp>
2020-06-30 14:14:17 +0900

    Userの認証

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       resources :pokemons
+      resources :users, only: [ :create ] do
+        collection do
+          post 'sign_in'
+          get 'me'
+        end
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_020943) do
+ActiveRecord::Schema.define(version: 2020_06_30_030601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,17 @@ ActiveRecord::Schema.define(version: 2020_06_04_020943) do
     t.integer "poke_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.string "token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
 end
